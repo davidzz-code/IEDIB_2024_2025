1c05c5f5bfeb22b6bfced97ed7d5dc06eed02f2b
davidzz-code <rrdavid08@gmail.com>
2024-12-01 03:18:29 +0100

    feat: Client and wen service created

--- 03 Programacion Machine Learning/T3 Gestión de proyectos/tarea_evaluable/penguins/penguins/penguins_service.py
+++ 03 Programacion Machine Learning/T3 Gestión de proyectos/tarea_evaluable/penguins/penguins/penguins_service.py
@@ -2,50 +2,50 @@ import pickle
 
 from flask import Flask, jsonify, request
 
-type_names = ['Adelie', 'Chinstrap', 'Gentoo']
+penguin_species = ['Adelie', 'Chinstrap', 'Gentoo']
 
-def predict_single(flower, sc, model):
-    flower_std = sc.transform([[flower['petal_length'],flower['petal_width']]])
-    y_pred = model.predict(flower_std)[0]
-    y_prob = model.predict_proba(flower_std)[0][y_pred]
+def predict_single(penguin, dv, model):
+    penguin_std = dv.transform([penguin])
+    y_pred = model.predict(penguin_std)[0]
+    y_prob = model.predict_proba(penguin_std)[0][y_pred]
     return (y_pred, y_prob)
 
-def predict(sc, model):
-    flower = request.get_json()
-    type, probability = predict_single(flower, sc, model)
+def predict(dv, model):
+    penguin = request.get_json()
+    specie, probability = predict_single(penguin, dv, model)
 
     result = {
-        'flower': type_names[type],
+        'penguin': penguin_species[specie],
         'probability': float(probability)
     }
     return jsonify(result)
 
-app = Flask('iris')
+app = Flask('penguins')
 
 
 @app.route('/predict_lr', methods=['POST'])
 def predict_lr():
     with open('models/lr.pck', 'rb') as f:
-        sc, model = pickle.load(f)
-    return predict(sc,model)
+        dv, model = pickle.load(f)
+    return predict(dv,model)
 
 @app.route('/predict_svm', methods=['POST'])
 def predict_svm():
     with open('models/svm.pck', 'rb') as f:
-        sc, model = pickle.load(f)
-    return predict(sc,model)
+        dv, model = pickle.load(f)
+    return predict(dv,model)
 
 @app.route('/predict_dt', methods=['POST'])
 def predict_dt():
     with open('models/dt.pck', 'rb') as f:
-        sc, model = pickle.load(f)
-    return predict(sc,model)
+        dv, model = pickle.load(f)
+    return predict(dv,model)
 
 @app.route('/predict_knn', methods=['POST'])
 def predict_knn():
     with open('models/knn.pck', 'rb') as f:
-        sc, model = pickle.load(f)
-    return predict(sc,model)
+        dv, model = pickle.load(f)
+    return predict(dv,model)
 
 
 if __name__ == '__main__':
